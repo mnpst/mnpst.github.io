973ef42ee47d9f6fa4ffc468ba597b8d654c3175
mto <mto@ableton.com>
2022-02-28 20:50:30 +0100

    update csv

--- script.js
+++ script.js
@@ -25,23 +25,18 @@ function renderData(data) {
     for (let j = 0; j < data[i].length; j++) {
       if (j === 0) {
         if (data[i][j] === '') return;
-        html += `<img src=${data[i][j]} width="200" height="200">`
-
+        html += `<img src=${data[i][0]} width="200" height="200">`
+      } else if (j === 1) {
+        html += `<p>${data[i][1]}</p>`
       } else if (j === 2) {
         html += `<a href="https://open.spotify.com/album/${data[i][5]}" target="_blank">${data[i][2]}</a>`
       } else if (j === 3) {
-        html += `<p>${data[i][8]}  -   ${data[i][3]}</p>`
-      } else if (j === 4) {
-        html += `<p id='rating'> Rating: ${data[i][4]}</p>`
-      } else if (j === 5) {
-      } else if (j === 6) {
+        html += `<p>${data[i][3]}  -   ${data[i][4]}</p>`
       } else if (j === 7) {
         html += `<p id='musicStyle'>${data[i][7]}</p>`
-      } else if (j === 8) {
-      } else if (j === 9) {
-        html += `<p id='decades'>${data[i][9]}</p>`
+      } else if (j === 15) {
+        html += `<p id='rating'> Rating: ${data[i][15]}</p>`
       } else {
-        html += `<p>${data[i][j]}</p>`
       }
 
     card.innerHTML = html;
