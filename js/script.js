7de9f05be91f033122ee0bf515e5f62d69dbbf47
mnpst <32594252+mnpst@users.noreply.github.com>
2023-12-09 09:11:19 +0100

    Start between styles

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function renderData(data) {
       } else if (j === 6) {
         html += `<p id='musicGenre'>${data[i][7]}</p>`
       } else if (j === 7) {
-        html += `<p id='musicStyle'>${data[i][8]} | ${data[i][9]}</p>`
+        html += `<p id='musicStyle'>${data[i][8]} * ${data[i][9]}</p>`
       } else if (j === 8) {
         html += `<p>${data[i][4]} (${data[i][17]})</p>`
       } else {
