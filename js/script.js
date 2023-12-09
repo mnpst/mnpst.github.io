416295c732fa29b324ab7389a5fdcb4fda71f577
mnpst <32594252+mnpst@users.noreply.github.com>
2023-12-09 11:55:22 +0100

    Try to make genre clickable for search

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function renderData(data) {
       } else if (j === 3) {
         html += `<p>${data[i][3]}</p>`
       } else if (j === 6) {
-        html += `<p id='musicGenre'>${data[i][7]}</p>`
+        html += `<a href="#" class='addfilter' data-filter="${data[i][7]}">${data[i][7]}</a>`
       } else if (j === 7) {
         html += `<p>${data[i][8]},${data[i][9]}</p>`
       } else if (j === 8) {
