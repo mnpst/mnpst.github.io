b144712c556326cd61e81209779d4a9f66b47854
mnpst <32594252+mnpst@users.noreply.github.com>
2023-12-09 11:59:56 +0100

    Add clickFilter to filter

--- js/filter.js
+++ js/filter.js
@@ -16,3 +16,10 @@ $("#filter").keyup(function() {
     }
   });
 });
+
+$(".addfilter").click(function() {
+  var filter = $(this).data("filter");
+  $("#result-text div").val(filter);
+  $("#result-text div").trigger("keyup");
+  return false;
+});
