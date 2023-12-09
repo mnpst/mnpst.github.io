2fecc26ba096a2286a19a7c97a2b8524ca2d2a44
mnpst <32594252+mnpst@users.noreply.github.com>
2023-12-09 09:43:13 +0100

    Add back main genre

--- js/script.js
+++ js/script.js
@@ -33,11 +33,13 @@ function renderData(data) {
       } else if (j === 3) {
         html += `<p>${data[i][3]}</p>`
       } else if (j === 6) {
-        html += `<p id='musicGenre'>${data[i][7]}</p>`
+        html += `<p id='musicGenre'>Genre: ${data[i][7]}</p>`
       } else if (j === 7) {
         html += `<p id='musicStyle'>${data[i][8]} * ${data[i][9]}</p>`
       } else if (j === 8) {
         html += `<p>${data[i][4]} (${data[i][17]})</p>`
+      } else if (j === 9) {
+        html += `<p id='bucket'>${data[i][6]}</p>`
       } else {
       }
 
