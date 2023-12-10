81e5c7b489861b23514deba72efd32e4ccf9245a
mnpst <32594252+mnpst@users.noreply.github.com>
2023-12-10 15:45:21 +0100

    Update Scripts

--- js/filter.js
+++ js/filter.js
@@ -1,18 +1,16 @@
 $("#filter").keyup(function() {
 
   var filter = $(this).val();
-  var gen = filterGenre();
   var dec = filterDecade();
     count = 0;
 
   $('#result-text div').each(function() {
 
-    if ($(this).text().search(new RegExp(filter, "i")) < 0 || $(this).text().search(new RegExp(gen, "i")) < 0 || $(this).text().search(new RegExp(dec, "i")) < 0 ) {
-      $(this).hide();
-
-    } else {
+    if ($(this).text().search(new RegExp(filter, "i")) > -1 && $(this).text().search(new RegExp(dec, "i")) > -1 ) {
       $(this).show();
       count++;
+    } else {
+      $(this).hide();
     }
   });
 });
--- js/script.js
+++ js/script.js
@@ -27,21 +27,19 @@ function renderData(data) {
         if (data[i][j] === '') return;
         html += `<img src=${data[i][0]} id= "img" width="400" height="400" class="center"><p></p>`;
       } else if (j === 1) {
-        html += `<a onclick="parseStyle('${data[i][1]}')" href="#">${data[i][1]}</a><p></p>`
+        html += `<a onclick="parseStyle('${data[i][1]}')" id = "albumArtist" href="#">${data[i][1]}</a><p></p>`
       } else if (j === 2) {
         html += `<a href="${data[i][5]}" target="_blank" id = "albumTitle">${data[i][2]}</a><p></p>`
       } else if (j === 3) {
-        html += `<a onclick="parseStyle('${data[i][3]}')" href="#">${data[i][3]}</a><p></p>`      
+        html += `Label: <a onclick="parseStyle('${data[i][3]}')" href="#">${data[i][3]}</a><p></p>`      
       } else if (j === 5) {
-        html += `<a onclick="parseStyle('${data[i][7]}')" id = "musicGenre" href="#">${data[i][7]}</a><p></p>`      
+        html += `<a onclick="parseStyle('${data[i][7]}')" id = "musicGenre" href="#">${data[i][7]}</a>`      
       } else if (j === 6) {
-        html += `<a onclick="parseStyle('${data[i][8]}')" id = "musicStyle" href="#">${data[i][8]}</a><p></p>`
+        html += `, <a onclick="parseStyle('${data[i][8]}')" id = "musicStyle" href="#">${data[i][8]}</a><p></p>`
       } else if (j === 7) {
-        html += `<a onclick="parseStyle('${data[i][9]}')" id = "musicStyle" href="#">${data[i][9]}</a>`
+        html += `<a onclick="parseStyle('${data[i][9]}')" id = "musicStyle" href="#">${data[i][9]}</a>, *${data[i][15]}*<p></p>`
       } else if (j === 8) {
         html += `<p>${data[i][4]} (${data[i][17]})</p>`
-      } else if (j === 9) {
-        html += `<p id='bucket'>${data[i][6]}</p>`
       } else {
       }
 
